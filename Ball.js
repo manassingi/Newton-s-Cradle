6920d078271b39acdd4735d121f2ac904127c52b
manassingi <70809272+manassingi@users.noreply.github.com>
2020-10-28 15:51:57 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -13,6 +13,7 @@ class ball
 		this.x=x;
 		this.y=y;
 		this.r=r
+		this.color=color(random(0,225),random(0,225),random(0,225))
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 
@@ -26,7 +27,7 @@ class ball
 			translate(pos1.x, pos1.y);
 			rectMode(CENTER)
 			strokeWeight(3);
-			fill(255,0,255)
+			fill(this.color)
 			ellipse(0,0,this.r, this.r);
 			pop()
 			
